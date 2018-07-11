d942fb62adefbc376c6b4fb75a0cb6e9941b8f30
ganchu <596761730@qq.com>
2018-07-11 10:21:46 +0800

    添加获取手机品牌+微信版本+网络类型
    添加获取手机品牌+微信版本+网络类型

--- getip.php
+++ getip.php
@@ -1,12 +1,59 @@
 <?php
 //error_reporting(0);
-//网上找不到完全OK的功能，所以自己写了个新的，获取地址功能需要文件在服务器
-//之前201805写的不能兼容本地正常显示，现在201807的可以兼容本地+服务器正常显示ip
-header("Content:Content-type:text/html;charset=utf-8");
-date_default_timezone_set("PRC");//设置时区
+//网上找不到完全OK的功能，所以自己写了个新的
+//更新日志：
+//20180711添加获取手机品牌+微信版本+网络类型
+//20180709修改可以兼容本地+服务器正常显示ip
+//201805取得客户端的ip、地理位置、浏览器、以及访问设备
 
-    // 作用取得客户端的ip、地理位置、浏览器、以及访问设备
-    class get_equipment_info{ 
+header("content-type:text/html;charset=utf-8");
+date_default_timezone_set("PRC");//设置时区
+        //获得访客手机品牌
+        function GetPhone(){
+            $user_agent = $_SERVER['HTTP_USER_AGENT'];
+            if (preg_match('/iPhone/i',$user_agent)) {
+                $brand = 'iPhone';
+            } else if (preg_match('/SAMSUNG/i',$user_agent)||preg_match('/Galaxy/i',$user_agent)||preg_match('/GT-/i',$user_agent)||preg_match('/SCH-/i',$user_agent)){
+                $brand = '三星';
+            } else if (preg_match('/Huawei/i',$user_agent)||preg_match('/Honor/i',$user_agent)||preg_match('/H60-/i',$user_agent)||preg_match('/H30-/i',$user_agent)) {
+                $brand = '华为';
+            } else if (preg_match('/Lenovo/i',$user_agent)) {
+                $brand = '联想';
+            } else if (preg_match('/mi/i',$user_agent)) {
+                $brand = '小米';
+            } else if (preg_match('/hm/i',$user_agent)) {
+                $brand = '红米';
+            } else if (preg_match('/Coolpad/i',$user_agent)||preg_match('/8190Q/i',$user_agent)||preg_match('/5910/i',$user_agent)) {
+                $brand = '酷派';
+            } else if (preg_match('/ZTE/i',$user_agent)||preg_match('/X9180/i',$user_agent)||preg_match('/N9180/i',$user_agent)) {
+                $brand = '中兴';
+            } else if (preg_match('/OPPO/i',$user_agent)||preg_match('/X9007/i',$user_agent)||preg_match('/X907/i',$user_agent)||preg_match('/X909/i',$user_agent)||preg_match('/R2017/i',$user_agent)||preg_match('/R811/i',$user_agent)||preg_match('/R821T/i',$user_agent)) {
+                $brand = 'OPPO';
+            } else if (preg_match('/HTC/i',$user_agent)||preg_match('/Desire/i',$user_agent)) {
+                $brand = 'HTC';
+            } else if (preg_match('/vivo/i',$user_agent)) {
+                $brand = 'vivo';
+            } else if (preg_match('/K-Touch/i',$user_agent)){
+                $brand = '天语';
+            } else if (preg_match('/Nubia/i',$user_agent)||preg_match('/NX50/i',$user_agent)||preg_match('/NX40/i',$user_agent)) {
+                $brand = '努比亚';
+            } else if (preg_match('/M045/i',$user_agent)||preg_match('/M355/i',$user_agent)||preg_match('/M032/i',$user_agent)) {
+                $brand = '魅族';
+            } else if (preg_match('/DOOV/i',$user_agent)) {
+                $brand = '朵唯';
+            } else if (preg_match('/GFIVE/i',$user_agent)) {
+                $brand = '基伍';
+            } else if (preg_match('/Gionee/i',$user_agent)||preg_match('/GN/i',$user_agent)) {
+                $brand = '金立';
+            } else if (preg_match('/HS-U/i',$user_agent)||preg_match('/HS-E/i',$user_agent)) {
+                $brand = '海信';
+            } else if (preg_match('/Nokia/i',$user_agent)) {
+                $brand = '诺基亚';
+            } else {
+                $brand = '其他品牌设备';
+            }
+            return $brand;
+        }
       ////获得访客浏览器类型
         function GetBrowser() {
             $user_OSagent = $_SERVER['HTTP_USER_AGENT'];
@@ -47,34 +94,42 @@ date_default_timezone_set("PRC");//设置时区
             } else {
                 $visitor_browser = "其它";
             }
-            //return $visitor_browser;
             return "浏览器:".$visitor_browser."|";
         }
         ////获得访客浏览器语言
         function GetLang()
-          {
-               if(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
-                   $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-                   $lang = substr($lang,0,5);
-                    if(preg_match("/zh-cn/i",$lang)){
-                       $lang = "简体中文";
-                    }elseif(preg_match("/zh/i",$lang)){
-                       $lang = "繁体中文";
-                    }else{
-                       $lang = "English";
-                    }
-                    return "语言:".$lang."|"; 
-               }else{
-                return "获取浏览器语言失败！";
+          {     
+            if(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
+                $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+                $lang = substr($lang,0,5);
+                if(preg_match("/zh-cn/i",$lang)){
+                    $lang = "简体中文";
+                }elseif(preg_match("/zh/i",$lang)){
+                    $lang = "繁体中文";
+                }else{
+                    $lang = "English";
                 }
+                    return "语言:".$lang."|"; 
+            }else{
+                return "获取浏览器语言失败！";
+            }
           }
        
         //获取客户端操作系统信息包括win10
         function GetOs(){
             $agent = $_SERVER['HTTP_USER_AGENT'];
             $os = false;
-        
-            if (preg_match('/win/i', $agent) && strpos($agent, '95'))
+            if(stristr($agent,'iPad')) {
+                $os = "iPad";
+            }else if(preg_match('/Android (([0-9_.]{1,3})+)/i',$agent,$version)) {
+                $os = "手机(Android ".$version[1].")";
+            }else if(stristr($agent,'Linux')){
+                $os = "电脑(Linux)";
+            }else if(preg_match('/iPhone OS (([0-9_.]{1,3})+)/i',$agent,$version)){
+                $os = "手机(iPhone ".$version[1].")";
+            }else if(preg_match('/Mac OS X (([0-9_.]{1,5})+)/i',$agent,$version)){
+                $os = "电脑(OS X ".$version[1].")";
+            }else if (preg_match('/win/i', $agent) && strpos($agent, '95'))
             {
                 $os = 'Windows 95';
             }
@@ -190,8 +245,29 @@ date_default_timezone_set("PRC");//设置时区
             }
             return "系统:".$os."|"; 
         }
-
-    }
+        //如果用户是微信打开，则获取版本号
+        function Wechat()
+        {   
+            $user_agent = $_SERVER['HTTP_USER_AGENT'];
+            if (strpos($user_agent, 'MicroMessenger')) {
+                preg_match('/.*?(MicroMessenger\/([0-9.]+))\s*/', $user_agent, $matches);
+                return '|微信:'.$matches[2];// 获取版本号
+            }
+        }
+        //获得访客网络类型，支持微信浏览器
+        function NetWork()
+        {   
+            $user_agent = $_SERVER['HTTP_USER_AGENT'];
+            if (preg_match('/wifi/i',$user_agent)) {
+                 return '|WIFI网络';
+            }elseif (preg_match('/4g/i',$user_agent)) {
+                return '|4g网络';
+            }elseif (preg_match('/3g/i',$user_agent)) {
+                return '|3g网络';
+            }elseif (preg_match('/2g/i',$user_agent)) {
+                return '|2g网络';
+            }
+        }
      // 获取 IP  地理位置
      // 淘宝IP接口
      // @Return: array
@@ -199,9 +275,9 @@ date_default_timezone_set("PRC");//设置时区
      // 希望这个api不要那么快失效，失效得找个直接获取ip值，或者curl网页获取ip
     function Getip() {
         $unknown = 'unknown';
-        if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown) ) {
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown) ) {
                 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } elseif ( isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown) ) { 
+        } elseif (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown) ) { 
                 $ip = $_SERVER['REMOTE_ADDR'];
                           
         }
@@ -215,12 +291,10 @@ date_default_timezone_set("PRC");//设置时区
     }
     //获取ip相关数据
     $data=Getip();
-    //实例化大类，获取ip外其他数据
-    $info = new get_equipment_info();
-
     //集合输出,最后加上换行，如果需要其他参数可以看$data里面的数据
-    $message=$data['data']['ip']."|".$data['data']['region'].$data['data']['city'].$data['data']['isp']."|".$info -> GetLang().$info -> GetOs().$info -> GetBrowser().date("Y-m-d H:i:s",time())."\r\n";
-     
+    $message=$data['data']['ip']."|".$data['data']['region'].$data['data']['city'].$data['data']['isp']."|".GetLang().GetOs().GetBrowser().date("Y-m-d H:i:s",time());
+    //2018711新增获取手机品牌，网络，微信版本
+    $message .= "|".GetPhone().NetWork().Wechat()."\r\n";
     echo $message;//113.70.47.90|广东佛山电信|浏览器语言:简体中文|系统:Windows 7|浏览器为Chrome|
  
     class Log {
